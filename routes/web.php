a8a81d5513a2fb462b2e997be5c99eccf5a54f91
naufal225 <naufalmarufashrori225@gmail.com>
2025-05-23 08:56:22 +0700

    membuuat routes dasar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\RuanganController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.dashboard.index');
 });
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('/users/import', [UserController::class, 'importForm'])->name('users.import.form');
+Route::post('/users/import', [UserController::class, 'import'])->name('users.import');
+
+Route::get('/ruangan', [RuanganController::class, 'index'])->name('ruangan.index');
+Route::get('/ruangan/create', [RuanganController::class, 'create'])->name('ruangan.create');
+Route::post('/ruangan', [RuanganController::class, 'store'])->name('ruangan.store');
+Route::get('/ruangan/{ruangan}/edit', [RuanganController::class, 'edit'])->name('ruangan.edit');
+Route::put('/ruangan/{ruangan}', [RuanganController::class, 'update'])->name('ruangan.update');
+Route::delete('/ruangan/{ruangan}', [RuanganController::class, 'destroy'])->name('ruangan.destroy');
+Route::get('/ruangan/import', [RuanganController::class, 'importForm'])->name('ruangan.import.form');
+Route::post('/ruangan/import', [RuanganController::class, 'import'])->name('ruangan.import');
